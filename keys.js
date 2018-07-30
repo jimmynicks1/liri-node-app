3b783bc42ada49b4d3dfb05053838b7270f6b7dd
jimmynicks1 <Jamesnickele@gmail.com>
2018-07-29 22:38:40 -0500

    add to git

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'K406cLgcTK59BfYMMs3apgc2R',
+  consumer_secret: 'Cf7pCwhB7Yi5PHQSbz8YVSYQN3mhhPqvwMDCgarGQWE8RrzluD',
+  access_token_key: '988749438448558080-7l44FCkRnmUZiYR66NSPLPo37GeuYwn',
+  access_token_secret: 'M3J3OjyIYoKqnuAcBn6fPm8JpRFfp9W86FwbjMCR3nSE9'
+}
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
